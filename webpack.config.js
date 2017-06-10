3d07e22d148d74ae4befa1f02448e3cf398a81df
idlewinn <edwinlin1987@gmail.com>
2017-06-10 02:59:03 -0700

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPluginConfig = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./dev/index.js",
@@ -20,7 +21,12 @@ module.exports = {
       title: "Playground",
       hash: true,
       template: "./dev/index.html"
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'dev/favicon.ico', to: 'dist/favicon.ico'
+      }
+    ])
   ],
   devServer: {
     port: 3000
