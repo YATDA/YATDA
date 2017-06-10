6c9e19fccc41371507e4948bfdd68f00e85380e5
idlewinn <edwinlin1987@gmail.com>
2017-06-10 01:24:03 -0700

    set up express server and deployment

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
