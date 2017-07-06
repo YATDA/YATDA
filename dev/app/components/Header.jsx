8e1dd3fd7237f3acfd39175267a53849f4cd5b0d
mmyung <mmyung806@gmail.com>
2017-07-05 22:50:39 -0700

    Create and add Header Component

---
+++ dev/app/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+    return <div className={props.className}>{props.title}</div>;
+}
+
+Header.propTypes = {
+    className: PropTypes.string,
+    title: PropTypes.string.isRequired,
+}
+
+Header.displayName = 'Header';
+
+export default Header;
