3dbd2c71bf9a6fa90429822d4352b07d9dfc641c
nelsont87 <nelsont87@gmail.com>
2017-07-11 12:17:09 -0700

    added link to console log todo item

--- dev/app/components/common/TodoItem.jsx
+++ dev/app/components/common/TodoItem.jsx
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+
 function TodoItem(props) {
+  function handleClick(event){
+    event.preventDefault();
+    console.log(props.name);
+  }
   return (
     <div className={props.className}>
-      {props.name}
+      <a href= {props.name} onClick={handleClick}>
+        {props.name}
+      </a>
     </div>
   );
 }
+
+
 
 TodoItem.propTypes = {
   className: PropTypes.string,
