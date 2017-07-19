8fc5053d1146b567a97d5a92eef409ffb9ed76bf
lauralin08 <lauralin08@gmail.com>
2017-07-18 20:48:36 -0700

    refactored component

--- dev/app/components/common/ButtonInput.jsx
+++ dev/app/components/common/ButtonInput.jsx
@@ -2,21 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 class ButtonInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(event) {
-  }
-
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
+      <form onSubmit={this.props.onSubmit}>
         <input
           type="button"
           value="Add item!"
-          onClick={this.onSubmit}
+          onClick={this.props.onSubmit}
         />
       </form>
     );
